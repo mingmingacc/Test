159cf87c0fb0b32a9f1fcfe26c37bef9a946ad85
明明acc <mingming@mingminaccdeMBP.lan>
2018-02-07 21:18:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mingming/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mingming.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        textView = (TextView) findViewById(R.id.textView);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("我爱你！");
+            }
+        });
+    }
+    //点击事件
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        btn = null;
+        textView = null;
+        btn.setOnClickListener(null);
+    }
+}
